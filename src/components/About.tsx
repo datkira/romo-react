31f730e77d4667fda7c2e6e3a4ee4783b798fa3d
Nguyen Tien Dat <abc.nguyentiendat@gmail.com>
2023-12-09 17:03:42 +0700

    test localStorage

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const About = () => {
+    const [localData, setLocalData] = useState('');
+
+    useEffect(() => {
+        // Set data into localStorage
+        window.localStorage.setItem('webview_test', 'This is stored in localStorage');
+
+        // Read data from localStorage
+        const data = window.localStorage.getItem('webview_test');
+
+        // Update state with data from localStorage
+        setLocalData(data as string);
+    }, []); // Run effect only once on component mount
+
     return (
         <div>
-            This is the about page.
+            <h2>About Page</h2>
+            <p>LocalStorage data: {localData}</p>
         </div>
     );
-}
+};
 
 export default About;
